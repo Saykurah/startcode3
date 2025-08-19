2437e20a31ae6d3e88195bc029de0bf548c4edf8
Lena Kleyn <r1001360@student.thomasmore.be>
2025-08-19 11:27:25 +0200

    final

--- index.js
+++ index.js
@@ -15,10 +15,20 @@ let priceFilter = undefined;
 renderData(data);
 
 function renderData(destinations) {
-    console.log("renderData", destinations);
     detailsDiv.innerHTML = destinations
+    const search = searchInput.value.toLowerCase();
+    const price = priceInput.value.toLowerCase();
+    let filtered = destinations.filter((destination) => {
+        const matchesSearch = destination.destinationName.toLowerCase().includes(search) ||
+            destination.destinationName.toLowerCase().includes(search);
+        const matchesPrice = !selectedPrice || destination.priceRangeEur === selectedPrice;
+        return matchesSearch && matchesPrice;
+    })
         .map(renderDestinationDetails)
         .join("");
+
+    renderDestinationThumbnail(destinations);
+
 
     //TODO fill detailsDiv.innerHTML with results of renderDestinationDetails(selectedDestination)
 
@@ -40,13 +50,17 @@ function renderDestinationThumbnail(destination) {
 
 function renderDestinationDetails(destination) {
     //TODO
+    const image = 'img/placeholder.jpeg';
     return `
         <div class="container-fluid">
 			<div class="row mb-2">
+			<h5 class="title">${destination.destinationName}</h5>
                 <div class="col-12 col-md-6">
-                    <img src="${image}" class="card-img-top object-fit-cover" />
+                    <img src="${destination.imgUrl}" class="card-img-top object-fit-cover" onerror="this.src='${image}"/>
                 </div>
                 <div class="col-12 col-md-6">
+                <p class="text">|${destination.city} in ${destination.country}</p>
+                <p class="text">${destination.description}</p>
                 </div>
              </div>
         </div>
